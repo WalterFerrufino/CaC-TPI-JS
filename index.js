42e4738bb64f753a62f02b4f5aa0e976f5cb7893
Walter Ferrufino <fmwalter@hotmail.com>
2022-10-18 16:49:07 -0300

    TPI terminado

---
+++ index.js
@@ -0,0 +1,54 @@
+// const valorTicket = 200;
+// const Estudiante = 0.2;
+// const Trainee = 0.5;
+// const Junior = 0.85;
+
+// var cantidadDeTickets = 0;
+
+// function totalAPagar(cantidadDeTickets, valorTicket, categoria) {
+//     const cantidadDeTickets = document.getElementById("cantidad");
+//     const categoria = document.getElementById("categoria");
+
+//     return cantidadDeTickets * valorTicket * categoria;
+
+
+// }
+
+
+// const totalAPagar = document.getElementById("totalAPagar");
+// totalAPagar.innerText = totalAPagar;
+
+
+
+function calcularPrecioConDescuento(cantidad, descuento) {
+    const porcentajePrecioConDescuento = 100 - descuento;
+    const precioConDescuento = 200*((cantidad * porcentajePrecioConDescuento) / 100);
+
+    return precioConDescuento;
+}
+
+function onClickButtonTotalAmount() {
+
+    const InputQuantity = document.getElementById("InputQuantity");
+    const quantityValue = InputQuantity.value;
+
+    const inputCategoria = document.getElementById("InputCategoria");
+    const valorCategoria = inputCategoria.value;
+
+    switch (valorCategoria) {
+        case "Estudiante":
+            descuento = 80;
+            break
+        case "Trainee":
+            descuento = 50;
+            break
+        case "Junior":
+            descuento = 15;
+            break;
+    }
+
+    const precioConDescuento = calcularPrecioConDescuento(quantityValue, descuento);
+
+    const resultP = document.getElementById("ResultP");
+    resultP.innerText = precioConDescuento;
+}
